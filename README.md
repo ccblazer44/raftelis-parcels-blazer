7e44408988e1a0f0f59445755dbb81907e02b847
Chris Blazer <ccblazer44@gmail.com>
2023-02-23 11:41:04 -0800

    project code

---
+++ README.md
@@ -0,0 +1,38 @@
+# raftelis-parcels-blazer
+
+## Parcels.txt not included
+I didn't want to put info in public repo, please put Parcels.txt in root directory.
+
+## ENVIRONMENT & DEPENDANCIES
+
+python -m venv ./venv
+
+.\venv\Scripts\activate
+
+pip install -r requirements.txt
+
+
+## RUN
+
+python .\parcels.py
+
+
+## USAGE
+
+all output formats will show currently sorted version of the dataset
+
+USAGE: 
+
+   print: prints data
+   
+   printalll: prints all data
+   
+   export: exports data to excel file
+   
+   grid: render in ag grid to view in browser
+   
+   sortname: sorts by first name
+   
+   sortadd: sorts by street name then street address
+   
+   quit: quit
---
+++ parcels.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import justpy as jp
+
+# create data frame
+temp = pd.read_csv('Parcels.txt', sep='|', header=0)
+df = pd.DataFrame(temp)
+
+
+# manual sanitization, would need better way to do this for larger data set.  Tailored to this specific project.
+df["ADDRESS"] = df["ADDRESS"].str.replace(" - ", " ")
+df["ADDRESS"] = df["ADDRESS"].str.replace(" A ", "A ")
+df["ADDRESS"] = df["ADDRESS"].str.replace(" B ", "B ")
+df["ADDRESS"] = df["ADDRESS"].str.replace(" C ", "C ")
+df["ADDRESS"] = df["ADDRESS"].str.replace(" D ", "D ")
+df["ADDRESS"] = df["ADDRESS"].str.replace(" F ", "F ")
+
+
+# simple google maps link
+# couldn't find a way to highlight the property lines like in the taxsifter links without a google maps api
+gmaps_str = "https://www.google.com/maps/search/"
+gmaps_str2 = ",+Mazama,+WA/"
+df["GMAPS"] = gmaps_str + df["ADDRESS"].str.replace(" ", "+") + gmaps_str2
+
+
+# create new columns for sorting
+df["STREET_NUMBER"] = df["ADDRESS"].str.split(" ", n=1).str[0]
+df["STREET_NAME"] = df["ADDRESS"].str.split(" ", n=1).str[1]
+
+df["FIRST_NAME"] = df["OWNER"].str.split(",", n=1).str[1]
+df["LAST_NAME"] = df["OWNER"].str.split(",", n=1).str[0]
+
+def basic_grid():
+    wp = jp.WebPage()
+    df.jp.ag_grid(a=wp)  # a=wp adds the grid to WebPage wp
+    return wp
+
+# if added columns are not wanted in output, can use this
+#     print(df[["PIN","ADDRESS","OWNER","MARKET_VALUE","SALE_DATE","SALE_PRICE","LINK","GMAPS"]])
+
+usage = 'USAGE: print: prints data, printalll: prints all data\n export: exports data to excel file, grid: render in ag grid to view in browser\n sortname: sorts by first name, sortadd: sorts by street name then street address, quit: quit'
+
+running = True
+
+while(running):
+    
+    print(usage)
+    x = input()
+    if(x == 'print'):
+        print("printing data: ")
+        print(df)
+    elif(x == "printall"):
+        print("printing all data: ")
+        print(df.to_string())
+    elif(x == 'sortname'):
+        df = df.sort_values(by=["FIRST_NAME"])
+        print("Data sorted by first name.")
+    elif(x == 'sortadd'):
+        df = df.sort_values(by=["STREET_NAME", "STREET_NUMBER"])
+        print("Data sorted by street name then street number.")
+    elif(x == "export"):
+        df.to_excel(r'.\Parcels.xlsx', index=False)
+        print("Exported to Parcels.xlsx")
+    elif(x == "grid"):
+        print("rendering grid...")
+        jp.justpy(basic_grid)
+    elif(x == "quit"):
+        running = False
+    else:
+        print("unrecognized input")
+
+
+
